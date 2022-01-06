3ff2d885c0e6e183bdd57b4bd63eb4fb1de3d9e9
Simon McConell <simonmcconnell@gmail.com>
2022-01-07 06:39:11 +0800

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,25 +1,40 @@
 defmodule Expkg.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/simonmcconnell/expkg"
+
   def project do
     [
       app: :expkg,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Generate release executables",
+      package: [
+        links: %{
+          "GitHub" => @source_url,
+          "Zig" => "https://ziglang.org"
+        },
+        licenses: ["MIT"]
+      ],
+      docs: [
+        main: "expkg",
+        source_url: @source_url,
+        source_ref: "v#{@version}",
+        extras: ["CHANGELOG.md"]
+      ]
     ]
   end
 
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Expkg, []}
     ]
   end
 
   defp deps do
-    [
-
-    ]
+    []
   end
 end
