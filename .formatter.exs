7c36c46f4ef3edaac93b013ce9dbff0d3610ad79
Simon McConnell <22566656+simonmcconnell@users.noreply.github.com>
2023-02-24 16:32:21 +0800

    housekeeping

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,4 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  import_deps: [:typed_struct]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
